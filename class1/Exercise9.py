c001ad24457f3b6f34f90c33674613a35adc1379
Gleydson <gleydsonmazioli@gmail.com>
2016-04-14 09:58:21 -0300

    Wrong commit. Fixing it

--- class1/Exercise9.py
+++ class1/Exercise9.py
@@ -4,10 +4,7 @@
 from ciscoconfparse import CiscoConfParse
 my_cisco_cfg = CiscoConfParse("cisco_ipsec.txt")
 
-out = my_cisco_cfg.find_objects_wo_child(parentspec=r"^crypto map CRYPTO", childspec=r"set transform-set AES-SHA")
+out = my_cisco_cfg.find_objects_w_child(parentspec=r"^crypto map CRYPTO", childspec=r"set pfs group2")
 for item in out:
     print item.text
-    out2 = my_cisco_cfg.find_objects_w_parents(parentspec=str(item.text), childspec=r"set transform-set")
-    for item2 in out2:
-	print item2.text
 
