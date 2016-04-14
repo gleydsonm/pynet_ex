577ef86a96679c40017f9a2264e80282166f88f6
Gleydson <gleydsonmazioli@gmail.com>
2016-04-14 09:55:09 -0300

    Exercise9.py

--- class1/Exercise9.py
+++ class1/Exercise9.py
@@ -4,5 +4,10 @@
 from ciscoconfparse import CiscoConfParse
 my_cisco_cfg = CiscoConfParse("cisco_ipsec.txt")
 
-out = my_cisco_cfg.find_objects_w_child(parentspec=r"^crypto map CRYPTO", childspec=r"set pfs group2")
-print out
+out = my_cisco_cfg.find_objects_wo_child(parentspec=r"^crypto map CRYPTO", childspec=r"set transform-set AES-SHA")
+for item in out:
+    print item.text
+    out2 = my_cisco_cfg.find_objects_w_parents(parentspec=str(item.text), childspec=r"set transform-set")
+    for item2 in out2:
+	print item2.text
+
