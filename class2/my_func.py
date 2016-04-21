e87f63984f2d3ec3c235bd01b2b6e3843af81ae9
Gleydson <gleydsonmazioli@gmail.com>
2016-04-21 07:37:14 -0300

    my_func.py

---
+++ class2/my_func.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Just a program/module that print hello
+# Gleydson Mazioli da Silva <gleydsonmazioli@gmail.com>
+
+def my_func():
+ print 'hello'
+
+if __name__ == "__main__":
+   my_func()
+
