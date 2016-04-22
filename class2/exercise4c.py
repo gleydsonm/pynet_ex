e7aca36258fda66d35ede5cc427f32895aaf45d6
root <root@randall.gms.com.br>
2016-04-22 10:46:25 -0300

    updated exercise4c and added done.txt

--- class2/exercise4c.py
+++ class2/exercise4c.py
@@ -15,6 +15,11 @@ ROUTERS.append('50.76.53.27|8061|galileo')
 
 def snmp_print_sysname(IP, PORT='161', COMMUNITY='public'):
     conn_dev = (IP, COMMUNITY, PORT)
+    snmp_data = snmp_extract(snmp_get_oid(conn_dev, oid='1.3.6.1.2.1.1.5.0'))
+    return snmp_data
+
+def snmp_print_sysdescr(IP, PORT='161', COMMUNITY='public'):
+    conn_dev = (IP, COMMUNITY, PORT)
     snmp_data = snmp_extract(snmp_get_oid(conn_dev, oid='1.3.6.1.2.1.1.1.0'))
     return snmp_data
     
@@ -27,6 +32,8 @@ def main():
     print "\n\n"
     print IP + ' ' + PORT + ' '+ COMMUNITY + '\n'
     output=snmp_print_sysname(IP, PORT, COMMUNITY)
+    print output
+    output=snmp_print_sysdescr(IP, PORT, COMMUNITY)
     print output
     print '-------------------------------------------------------------'
 
