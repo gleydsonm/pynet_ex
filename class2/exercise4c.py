cd4823703a8ea17538b6153393aa73e340e33f40
root <root@randall.gms.com.br>
2016-04-22 10:41:24 -0300

    added exercise4c

---
+++ class2/exercise4c.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+'''
+Exercise 4c - as also solve exercise 4b (module snmp_helper) import
+I chose to use list to inform parameters and split to distribute them
+among variables. That make easy to convert the program to be command line 
+driven
+
+The separator is '|', so we can handle IPV6 addresses and is more easy to
+see than CSV separator ','
+'''
+from snmp_helper import snmp_get_oid,snmp_extract
+ROUTERS=[]
+ROUTERS.append('50.76.53.27|7961|galileo')
+ROUTERS.append('50.76.53.27|8061|galileo')
+
+def snmp_print_sysname(IP, PORT='161', COMMUNITY='public'):
+    conn_dev = (IP, COMMUNITY, PORT)
+    snmp_data = snmp_extract(snmp_get_oid(conn_dev, oid='1.3.6.1.2.1.1.1.0'))
+    return snmp_data
+    
+
+def main():
+  for ROUTER in ROUTERS:
+    IP=ROUTER.split('|')[0]
+    PORT=ROUTER.split('|')[1]
+    COMMUNITY=ROUTER.split('|')[2]
+    print "\n\n"
+    print IP + ' ' + PORT + ' '+ COMMUNITY + '\n'
+    output=snmp_print_sysname(IP, PORT, COMMUNITY)
+    print output
+    print '-------------------------------------------------------------'
+
+if __name__ == "__main__":
+    main()
+
+quit()
+
