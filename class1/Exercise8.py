92caac789dd4ccf90c1f9e6ca21684590d249c68
root <root@randall.gms.com.br>
2016-04-23 22:29:15 -0300

    Code cosmetics and code improvements

--- class1/Exercise8.py
+++ class1/Exercise8.py
@@ -2,12 +2,19 @@
 ''' Exercise 8 - PyNet Python '''
 
 from ciscoconfparse import CiscoConfParse
-my_cisco_cfg = CiscoConfParse("cisco_ipsec.txt")
-var1 = my_cisco_cfg.find_objects(r"^crypto map CRYPTO")
 
-for item in var1:
-    print item.text
-    for children in item.children:
-	print children.text
-    print '-----------------------'
+def main():
+    '''
+    Main Function
+    '''
+    my_cisco_cfg = CiscoConfParse("cisco_ipsec.txt")
+    var1 = my_cisco_cfg.find_objects(r"^crypto map CRYPTO")
+    for item in var1:
+        print item.text
+        for children in item.children:
+            print children.text
+        print '-----------------------'
 
+
+if __name__ == '__main__':
+    main()
