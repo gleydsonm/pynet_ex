acc71d8a5a1e7030f05cdf9b27a77509a8644d50
root <root@randall.gms.com.br>
2016-04-23 22:25:26 -0300

    Code cosmetics and code improvements

--- class1/Exercise7.py
+++ class1/Exercise7.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-''' Exercise 7 - PyNet Python 
+''' Class 1 - Exercise 7 - PyNet + Ansible
 It will just read compatible JSON and YAML files created on /tmp directory, and
 after that show them using list format
 '''
@@ -8,19 +8,25 @@ import yaml
 import json
 import pprint
 
-# As I run on a Unix variant, /tmp follow the FHS2 standard and is a right place to 
-# create and possible forgot about temporary files 
+# As I run on a Unix variant, /tmp follow the FHS2 standard and is a
+# right place to create and possible forgot about temporary files
 
-print 'The output of the loaded YAML file, is\n'
-with open("/tmp/Exercise6.yaml") as file:
-  new_list = yaml.load(file)
-  pprint.pprint(new_list)
+def main():
+    '''
+    Main function
+    '''
+    print 'The output of the loaded YAML file, is\n'
+    with open("/tmp/Exercise6.yaml") as fhandler:
+        new_list = yaml.load(fhandler)
+        pprint.pprint(new_list)
 
-print '\n---------\n'
-  
-print 'The output of the loaded JSON file, is\n'
-with open("/tmp/Exercise6.json") as file:
-  new_list=json.load(file)
-  pprint.pprint(new_list)
+    print '\n---------\n'
 
-exit
+    print 'The output of the loaded JSON file, is\n'
+    with open("/tmp/Exercise6.json") as fhandler:
+        new_list = json.load(fhandler)
+        pprint.pprint(new_list)
+
+if __name__ == "__main__":
+    main()
+
