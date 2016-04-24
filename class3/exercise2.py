8b82d8de6457dfe3263bef30ea1bebd86c47f7e1
root <root@randall.gms.com.br>
2016-04-24 20:55:18 -0300

    Added exercise2 to the repository. I'm using a off-line data collection structure to save data on yaml or json file and allowing it to run using cron or anacron - saving system resources

--- class3/exercise2.py
+++ class3/exercise2.py
@@ -2,12 +2,17 @@
 '''
 Python + Ansible - Class 3 - Exercise 2
 Gleydson Mazioli da Silva <gleydsonmazioli@gmail.com>
+
+I created this program with a different concept: Data saving and load using
+a yaml or json file. So the system save system resources and can be run throught
+a cron or anacron job.
 '''
 
 import snmp_helper
 import yaml
 import json
 import sys
+import pygal
 
 # pylint: disable=C0103
 # pylint: disable=line-too-long
@@ -40,12 +45,12 @@ def save_data(l_var):
 
 
 
-def load_saved_data(l_var, l_default):
+def load_saved_data(l_default):
     '''
     Load previous saved data
     '''
     try:
-        with open('exercise2-1.'+file_fmt, 'r') as fhandler:
+        with open('exercise3-2.'+file_fmt, 'r') as fhandler:
             if file_fmt == 'yaml':
                 file_data = yaml.load(fhandler)
             elif file_fmt == 'json':
@@ -56,7 +61,7 @@ def load_saved_data(l_var, l_default):
         if verbose:
             print 'File not found: exercise3-2.'+file_fmt
         return l_default
-    return file_data[0][l_var]
+    return file_data
 
 
 def get_snmp_data(router, snmp_user, miboid):
@@ -67,15 +72,38 @@ def get_snmp_data(router, snmp_user, miboid):
     return snmp_data
 
 
-def diff_values(val1, val2):
+def generate_graphic(l_data):
     '''
-    Do the subtraction to get the delta value of time series data
+    Generate a SVG graphic using data passed as an argument
     '''
-    l_result = val2-val1
-    # Take care of counter interfaces roll over or after a reboot
-    if l_result < 0:
-        l_result = val1
-    return l_result
+    graph_stats = {
+        "in_octets": [],
+        "out_octets": [],
+        "in_ucast_pkts": [],
+        "out_ucast_pkts": []
+    }
+
+    for l_label in ("in_octets", "out_octets", "in_ucast_pkts", "out_ucast_pkts"):
+        l_old_value = 0
+        for i in range(0, len(l_data)):
+            l_value = l_data[i][l_label]
+            if l_old_value == 0:
+                l_diff = 0
+            else:
+                l_diff = int(l_value)-int(l_old_value)
+            if verbose:
+                print 'xxxxx: %s, diff: %s, (old: %s)' % (l_value, l_diff, l_old_value)
+            graph_stats[l_label].append(l_diff)
+            l_old_value = l_value
+    if verbose:
+        print graph_stats
+    line_chart = pygal.Line()
+    line_chart.title = 'Input/Output bytes and Unicast'
+    line_chart.add('InBytes', graph_stats['in_octets'])
+    line_chart.add('OutBytes', graph_stats['out_octets'])
+    line_chart.add('InUnicast', graph_stats['in_ucast_pkts'])
+    line_chart.add('OutUnicast', graph_stats['out_ucast_pkts'])
+    line_chart.render_to_file('exercise2.svg')
 
 
 def main():
@@ -87,14 +115,25 @@ def main():
     if_in_octets = get_snmp_data(my_host, snmp_user, '1.3.6.1.2.1.2.2.1.10.5')
     if_out_octets = get_snmp_data(my_host, snmp_user, '1.3.6.1.2.1.2.2.1.16.5')
     if_in_ucast_pkts = get_snmp_data(my_host, snmp_user, '1.3.6.1.2.1.2.2.1.11.5')
-    if_out_ucast_pkts = get_snmp_data(my_host, snmp_user, '1.3.6.1.2.1.2.2.1.17.')
+    if_out_ucast_pkts = get_snmp_data(my_host, snmp_user, '1.3.6.1.2.1.2.2.1.17.5')
     print 'Using file format: %s' % (file_fmt)
 
-    old_if_ifdescr = load_saved_data('if_descr', if_ifdescr)
-    old_if_in_octets = load_saved_data('if_in_octets', if_in_octets)
-    old_if_out_octets = load_saved_data('if_out_octets', if_out_octets)
-    old_if_in_ucast_pkts = load_saved_data('if_in_ucast_pkts', if_in_ucast_pkts)
-    old_if_out_ucast_pkts = load_saved_data('if_out_ucast_pkts', if_out_ucast_pkts)
+    old_data_list = load_saved_data(0)
+    # pylint: disable=maybe-no-member
+    if old_data_list == 0:
+        old_if_ifdescr = if_ifdescr
+        old_if_in_octets = if_in_octets
+        old_if_out_octets = if_out_octets
+        old_if_in_ucast_pkts = if_in_ucast_pkts
+        old_if_out_ucast_pkts = if_out_ucast_pkts
+        data_list = range(0)
+    else:
+        old_if_ifdescr = old_data_list[-1]['ifdescr']
+        old_if_in_octets = old_data_list[-1]['in_octets']
+        old_if_out_octets = old_data_list[-1]['out_octets']
+        old_if_in_ucast_pkts = old_data_list[-1]['in_ucast_pkts']
+        old_if_out_ucast_pkts = old_data_list[-1]['out_ucast_pkts']
+        data_list = old_data_list
 
     if verbose:
         print 'IfDescr: %s (last: %s)' % (if_ifdescr, old_if_ifdescr)
@@ -103,21 +142,20 @@ def main():
         print 'In Ucast %s (last: %s)' % (if_in_ucast_pkts, old_if_in_ucast_pkts)
         print 'Out Ucast %s (last: %s)' % (if_out_ucast_pkts, old_if_out_ucast_pkts)
 
-    result = diff_values(old_if_ifdescr, if_ifdescr)
-    print result
 
     # Array preparation to save data
-    data_list = range(0)
     data_list.append({})
-    data_list[0]['if_descr'] = if_ifdescr
-    data_list[0]['in_octets'] = if_in_octets
-    data_list[0]['out_octets'] = if_out_octets
-    data_list[0]['in_ucast_pkts'] = if_in_ucast_pkts
-    data_list[0]['out_ucast_pkts'] = if_out_ucast_pkts
+    data_list[-1]['ifdescr'] = if_ifdescr
+    data_list[-1]['in_octets'] = if_in_octets
+    data_list[-1]['out_octets'] = if_out_octets
+    data_list[-1]['in_ucast_pkts'] = if_in_ucast_pkts
+    data_list[-1]['out_ucast_pkts'] = if_out_ucast_pkts
 
     save_data(data_list)
 
-    print '----------------------------'
+    generate_graphic(data_list)
+    if verbose:
+        print '----------------------------'
 
 if __name__ == "__main__":
     main()
