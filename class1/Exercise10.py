824a2218ab5371cd38f343ad41da3f1293163e97
root <root@randall.gms.com.br>
2016-04-23 22:41:04 -0300

    Exercise10.py

--- class1/Exercise10.py
+++ class1/Exercise10.py
@@ -2,12 +2,19 @@
 ''' Exercise 10 - PyNet Python '''
 
 from ciscoconfparse import CiscoConfParse
-my_cisco_cfg = CiscoConfParse("cisco_ipsec.txt")
 
-out = my_cisco_cfg.find_objects_wo_child(parentspec=r"^crypto map CRYPTO", childspec=r"set transform-set AES-SHA")
-for item in out:
-    print item.text
-    out2 = my_cisco_cfg.find_objects_w_parents(parentspec=str(item.text), childspec=r"set transform-set")
-    for item2 in out2:
-	print item2.text
+def main():
+    '''
+    Main Function
+    '''
+    my_cisco_cfg = CiscoConfParse("cisco_ipsec.txt")
+    out = my_cisco_cfg.find_objects_wo_child(parentspec=r"^crypto map CRYPTO", childspec=r"set transform-set AES-SHA")
+    for item in out:
+        print item.text
+        out2 = my_cisco_cfg.find_objects_w_parents(parentspec=str(item.text), childspec=r"set transform-set")
+        for item2 in out2:
+            print item2.text
 
+
+if __name__ == "__main__":
+    main()
