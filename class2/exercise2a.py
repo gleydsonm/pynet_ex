ef4c50d89b229cc5991a672daa93a10dff6533af
root <root@randall.gms.com.br>
2016-04-24 07:53:16 -0300

    Fixed syntax typos and cosmetics

--- class2/exercise2a.py
+++ class2/exercise2a.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-# Python + ansible - Exercise 2a
-# I'm using on this example a method to check for login failures. Also, the non
-# sensitive data are received directly by command line parameters
-#
-# Gleydson Mazioli da Silva <gleydsonmazioli@gmail.com>
+'''
+Python + ansible - Exercise 2a
+I'm using on this example a method to check for login failures. Also, the non
+sensitive data are received directly by command line parameters
+
+Gleydson Mazioli da Silva <gleydsonmazioli@gmail.com>
+'''
 
 import sys
 import time
@@ -13,66 +15,78 @@ import getpass
 import re
 
 # Global variables
-TELNET_TIMEOUT=5
+TELNET_TIMEOUT = 5
 
-# Connect to the remote host
-def telnet_connect(l_ip_addr,l_port):
-        try:    
-           return telnetlib.Telnet(l_ip_addr, l_port, TELNET_TIMEOUT)
-        except socket.error:
-            sys.exit("Telnet: Connection refused")
-        except socket.timeout:
-            sys.exit("Telnet: Connection timed out")
+def telnet_connect(l_ip_addr, l_port):
+    '''
+    Connect to the remote host
+    '''
+    try:
+        return telnetlib.Telnet(l_ip_addr, l_port, TELNET_TIMEOUT)
+    except socket.error:
+        sys.exit("Telnet: Connection refused")
+    except socket.timeout:
+        sys.exit("Telnet: Connection timed out")
 
 def telnet_login(remote_conn, username, password):
-        output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
-        remote_conn.write(username + '\n')
-        output += remote_conn.read_until("assword:", TELNET_TIMEOUT)
-        remote_conn.write(password + '\n')
-        return output
+    '''
+    Login on the remote host
+    '''
+    output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+    remote_conn.write(username + '\n')
+    output += remote_conn.read_until("assword:", TELNET_TIMEOUT)
+    remote_conn.write(password + '\n')
+    return output
 
 
 def telnet_logout(remote_conn):
-        remote_conn.close()
+    '''
+    Logout from the remote host
+    '''
+    remote_conn.close()
 
 def send_command(remote_conn, cmd):
-        cmd = cmd.rstrip()
-        remote_conn.write(cmd + "\n")
-        time.sleep(1)
-        return remote_conn.read_very_eager()
+    '''
+    Send commands to the remote host
+    '''
+    cmd = cmd.rstrip()
+    remote_conn.write(cmd + "\n")
+    time.sleep(1)
+    return remote_conn.read_very_eager()
 
 print sys.argv
 if len(sys.argv) < 4:
-   sys.exit('Error: Requested parameters: ' + sys.argv[0] + ' [dst_ip] [port] [username]')
+    sys.exit('Parameters: ' + sys.argv[0] + ' [dst_ip] [port] [username]')
 
 def main():
-    # Read connections parameters from the command line
-    telnet_ip=sys.argv[1]
-    telnet_port=sys.argv[2]
-    telnet_username=sys.argv[3]
-    telnet_password=getpass.getpass()
+    '''
+    Read connections parameters from the command line
+    '''
+    telnet_ip = sys.argv[1]
+    telnet_port = sys.argv[2]
+    telnet_username = sys.argv[3]
+    telnet_password = getpass.getpass()
 
-    conn_handle=telnet_connect(telnet_ip, telnet_port)
-    output=telnet_login(conn_handle, telnet_username, telnet_password)
+    conn_handle = telnet_connect(telnet_ip, telnet_port)
+    output = telnet_login(conn_handle, telnet_username, telnet_password)
     time.sleep(1)
-    output=conn_handle.read_very_eager()
+    output = conn_handle.read_very_eager()
 
-    expr=re.compile('#')
+    expr = re.compile('#')
     if not expr.search(output):
         print 'Incorrect login or password. Exiting'
         sys.exit()
     else:
         print 'We are in!'
 
-    output=conn_handle.read_very_eager()
+    output = conn_handle.read_very_eager()
     print output
-    send_command(conn_handle,'terminal lenght 0')
+    send_command(conn_handle, 'terminal lenght 0')
     time.sleep(1)
-    output = send_command(conn_handle,'show ip int brief')
+    output = send_command(conn_handle, 'show ip int brief')
     print output
 
     telnet_logout(conn_handle)
-    #    
 
 if __name__ == '__main__':
     main()
