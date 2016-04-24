b78c4f39106f76dd07b7b0e140a38d3ad4254c5d
root <root@randall.gms.com.br>
2016-04-23 22:45:28 -0300

    Code cosmetics and code improvements

--- class2/exercise1a.py
+++ class2/exercise1a.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
-# Exercise 1a - Class 2 - Check module loading
-# Changes to use exceptions to check if module exist. So this could be used to
-# make the programa compatible with more than one specific module if the
-# preferred one isn't found on the system (like pysnmp and netsnmp)
-# 
-# Gleydson Mazioli da Silva <gleydsonmazioli@gmail.com>
+'''
+Exercise 1a - Class 2 - Check module loading
+Changes to use exceptions to check if module exist. So this could be used to
+make the programa compatible with more than one specific module if the
+preferred one isn't found on the system (like pysnmp and netsnmp)
+
+Gleydson Mazioli da Silva <gleydsonmazioli@gmail.com>
+'''
 
 try:
     import pysnmp
 except ImportError:
-     print 'pysnmp module not installed. Please install it using your package manager or PIP.'
-     quit()
+    print 'pysnmp module not installed. Please install it using PIP.'
+    quit()
 
 try:
     import paramiko
 except ImportError:
-     print 'paramiko module not installed. Please install it using your package manager or PIP.'
-     quit()
+    print 'paramiko module not installed. Please install it using PIP.'
+    quit()
 
 print 'All modules sucessfully loaded. Doing something useful after this'
 
