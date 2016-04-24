2c7b1bd7c06fa04280f1c683aae3d7b2ec570b66
root <root@randall.gms.com.br>
2016-04-24 08:06:29 -0300

    Some fixes and cosmetics

--- class2/exercise4c.py
+++ class2/exercise4c.py
@@ -2,40 +2,50 @@
 '''
 Exercise 4c - as also solve exercise 4b (module snmp_helper) import
 I chose to use list to inform parameters and split to distribute them
-among variables. That make easy to convert the program to be command line 
+among variables. That make easy to convert the program to be command line
 driven
 
 The separator is '|', so we can handle IPV6 addresses and is more easy to
 see than CSV separator ','
 '''
-from snmp_helper import snmp_get_oid,snmp_extract
-ROUTERS=[]
-ROUTERS.append('50.76.53.27|7961|galileo')
-ROUTERS.append('50.76.53.27|8061|galileo')
+from snmp_helper import snmp_get_oid, snmp_extract
 
-def snmp_print_sysname(IP, PORT='161', COMMUNITY='public'):
-    conn_dev = (IP, COMMUNITY, PORT)
+def snmp_print_sysname(l_ip, l_port='161', l_community='public'):
+    '''
+    Print snmp sysname
+    '''
+    conn_dev = (l_ip, l_community, l_port)
     snmp_data = snmp_extract(snmp_get_oid(conn_dev, oid='1.3.6.1.2.1.1.5.0'))
     return snmp_data
 
-def snmp_print_sysdescr(IP, PORT='161', COMMUNITY='public'):
-    conn_dev = (IP, COMMUNITY, PORT)
+def snmp_print_sysdescr(l_ip, l_port='161', l_community='public'):
+    '''
+    Print snmp sysdescr
+    '''
+    conn_dev = (l_ip, l_community, l_port)
     snmp_data = snmp_extract(snmp_get_oid(conn_dev, oid='1.3.6.1.2.1.1.1.0'))
     return snmp_data
-    
+
 
 def main():
-  for ROUTER in ROUTERS:
-    IP=ROUTER.split('|')[0]
-    PORT=ROUTER.split('|')[1]
-    COMMUNITY=ROUTER.split('|')[2]
-    print "\n\n"
-    print IP + ' ' + PORT + ' '+ COMMUNITY + '\n'
-    output=snmp_print_sysname(IP, PORT, COMMUNITY)
-    print output
-    output=snmp_print_sysdescr(IP, PORT, COMMUNITY)
-    print output
-    print '-------------------------------------------------------------'
+    '''
+    Main function
+    '''
+    routers = []
+    routers.append('50.76.53.27|7961|galileo')
+    routers.append('50.76.53.27|8061|galileo')
+
+    for router in routers:
+        my_ip = router.split('|')[0]
+        my_port = router.split('|')[1]
+        my_community = router.split('|')[2]
+        print "\n\n"
+        print '%s %s %s \n' % (my_ip, my_port, my_community)
+        output = snmp_print_sysname(my_ip, my_port, my_community)
+        print output
+        output = snmp_print_sysdescr(my_ip, my_port, my_community)
+        print output
+        print '-------------------------------------------------------------'
 
 if __name__ == "__main__":
     main()
