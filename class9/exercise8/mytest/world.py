cc87f06488a83be7eb7caaf7c39f5f181e4f87f0
Gleydson <gleydsonmazioli@gmail.com>
2016-06-05 14:01:09 -0300

    Adding world.py

---
+++ class9/exercise8/mytest/world.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+'''
+Class 9 - Exercise 8
+'''
+#pylint: disable=invalid-name
+
+def func1(param='World'):
+    '''
+    Print a parameter when a function is called
+    '''
+    print param
+
+class MyClass(object):
+    '''
+    MyClass - Main class of Exercise 8
+    '''
+    def __init__(self, x='param1', y='param2', z='param3'):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def hello(self):
+        '''
+        Hello method
+        '''
+        return 'Hello: {} {}'.format(self.x, self.z)
+
+    def not_hello(self):
+        '''
+        Not Hello method
+        '''
+        return 'Not Hello: {} {}'.format(self.y, self.z)
+
+
+class MyChildClass(MyClass):
+    '''
+    MyChildClass
+    '''
+    def __init__(self, x='param1', y='param2', z='param3'):
+        self.x = x
+        self.y = y
+        self.z = z + ' (thats all folks)'
+        MyClass.__init__(self, x, y, z)
+
+    def hello(self):
+        '''
+        MyChildClas hello method, that override hello from MyClass
+        '''
+        return 'ChildClass: {} {}'.format(self.x, self.z)
+
+if __name__ == '__main__':
+    print 'world is just a module, you need to import it'
