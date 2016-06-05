ceb93bb5d350b5215539f44b565aea46a894e927
Gleydson <gleydsonmazioli@gmail.com>
2016-06-05 13:57:33 -0300

    Adding world.py

---
+++ class9/exercise2/mytest/world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+Class 9 - Exercise 2
+'''
+
+def func1(param='World'):
+    '''
+    Print a parameter when a function is called
+    '''
+    print param
+
+
+if __name__ == '__main__':
+    print 'world is just a module, you need to import it'
