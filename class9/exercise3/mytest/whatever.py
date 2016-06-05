69217ec75233b4ee587ad5c1e9f73d85b2c15c30
Gleydson <gleydsonmazioli@gmail.com>
2016-06-05 13:57:53 -0300

    Adding mytest

---
+++ class9/exercise3/mytest/whatever.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+Class 9 - Exercise 3
+'''
+
+def func3(param='Whatever'):
+    '''
+    Print a parameter when a function is called
+    '''
+    print param
+
+
+if __name__ == '__main__':
+    print 'whatever is just a module, you need to import it'
