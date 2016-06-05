2295714a52afd41e489a73418e5b970876557e24
Gleydson <gleydsonmazioli@gmail.com>
2016-06-05 14:01:00 -0300

    Adding simple.py

---
+++ class9/exercise7/mytest/simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+Class 9 - Exercise 7
+'''
+
+def func2(param='Simple'):
+    '''
+    Print a parameter when a function is called
+    '''
+    print param
+
+
+if __name__ == '__main__':
+    print 'simple is just a module, you need to import it'
