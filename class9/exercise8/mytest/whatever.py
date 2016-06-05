6b15b111ff45e0aee64b7b6a7069287765009f61
Gleydson <gleydsonmazioli@gmail.com>
2016-06-05 14:01:09 -0300

    Adding whatever.py

---
+++ class9/exercise8/mytest/whatever.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+'''
+Class 9 - Exercise 8
+'''
+
+def func3(param='Whatever'):
+    '''
+    Print a parameter when a function is called
+    '''
+    print param
+
+if __name__ == '__main__':
+    print 'whatever is just a module, you need to import it'
